75630f92c7fbb487cd0fb694d89423de850335dd
Alejandro <addrriel@hotmail.com>
2024-10-30 23:47:38 -0500

    Introducción a Python

---
+++ my_program.py
@@ -0,0 +1,75 @@
+#Primer comentario: print para imprimir los resultados
+'''
+Esto es un comentario
+de varias líneas
+que el intérprete ignorará.
+'''
+print(3+4)
+print(4+9)
+print(10+5)
+print("Operadores Lógicos")
+#Addition +
+print(3+2)
+#Subtraction - 
+print(4-2)
+#Multiplication *
+print (4*2)
+#division /
+print (3/4)
+#Floor division // - Es división entera
+print(9 // 2)
+print ("Si tu habitación tiene nueve metros de altura, podrías colocar cuatro armarios de 2 metros de altura uno encima de otro.")
+
+#Modulus % Módulos
+print(9 % 2)
+print("Si tu habitación tiene nueve metros de altura, podrías colocar cuatro armarios de 2 metros de altura uno encima de otro. Quedaría un metro de espacio vertical.")
+
+#Exponentiation ** Exponencial
+print (3**4)
+print("3 ** 4 equivale a 3 * 3 * 3 * 3.")
+
+#con Control + podemos hacer zoom al visual
+#Primer Cuestionario
+print("Cuestionario")
+print(117 + 986)
+print(199 - 981)
+
+print("Ejercicio 2")
+print(3 + 4 + 5 - 6)
+
+print("Ejercicio 3")
+print(3+4) 
+print(3 + 4) 
+
+#-------------------------------------------------------------------------
+
+print("Sección 3, Variables y más")
+#Variables
+print("Ejercicio 1 Variables")
+#variable= valor
+spent= 3
+donated= 4
+#variable = variable1 + variable 2
+totalAmount = spent + donated
+print(totalAmount)
+
+print("Ejercicio 2 Variable")
+items= 10
+price= 2
+total_price= items * price
+print(total_price)
+
+#Consejo 1 - Impresión de múltiples variables 
+#También es posible imprimir múltiples variables en una sola función de impresión utilizando comas entre las variables. La última línea del código siguiente contiene un ejemplo:
+print("Consejo 1, se puede imprimir múltiples variables")
+items2 = 10
+price2 = 2
+ 
+total_price2 = items2 * price2
+ 
+# Below we print out three variables
+print(total_price2, items2, price2) 
+
+#Consejo 2: cls es para limpiar la terminar
+print("Consejo2")
+print("cls en la termina sirve para limpiar la terminal")
